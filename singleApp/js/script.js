4062aa33523d415435df798be252c15a7ebb4484
newbiecse <newbiecse@gmail.com>
2015-06-09 13:00:35 +0700

    add service

--- singleApp/js/script.js
+++ singleApp/js/script.js
@@ -1,6 +1,31 @@
 	// create the module and name it scotchApp
 	var scotchApp = angular.module('scotchApp', ['ngRoute']);
 
+	
+	// services
+	scotchApp.service('myService', function () {
+		var listData = {};
+		this.setListData = function(_listData) {
+			listData = _listData;
+		}
+		this.getListData = function() {
+			return listData;
+		}
+	});	
+	/*
+	scotchApp.factory('myService', function () {
+		var listData = {};
+		this.setListData = function(_listData) {
+			listData = _listData;
+		}
+		this.getListData = function() {
+			return listData;
+		}
+	});
+	*/
+	
+	
+	
 	// configure our routes
 	scotchApp.config(function($routeProvider) {
 		$routeProvider
@@ -29,13 +54,12 @@
 	});
 
 	// create the controller and inject Angular's $scope
-	scotchApp.controller('mainController', function($scope, $http) {
+	scotchApp.controller('mainController', ['$scope', 'myService', '$http', function($scope, myService, $http) {
 		// create a message to display in our view
 		$scope.message = 'Everyone come and see how good I look!';
 		
-		if($scope.listObj == null || typeof $scope.listObj == "undefined") {
-			$scope.listObj = {};
-		}
+		
+		$scope.listObj = myService.getListData();
 		
 		$scope.loadAjax = function (){
 			
@@ -47,6 +71,8 @@
 				// this callback will be called asynchronously
 				// when the response is available
 				$scope.listObj = data;
+				// store data to service
+				myService.setListData(data);
 				$scope.loading = false;
 			  }).
 			  error(function(data, status, headers, config) {
@@ -55,7 +81,8 @@
 				alert(data);
 			  });			
 		}
-	});
+		
+	}]);
 
 	scotchApp.controller('aboutController', function($scope) {
 		$scope.message = 'Look! I am an about page.';
